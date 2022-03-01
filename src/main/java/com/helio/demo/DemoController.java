000f09d767e2e9ad4b7b215f37533cf9c3962cf7
Devin Austin <devin.austin@gmail.com>
2022-03-01 13:50:49 -0700

    initial commit

---
+++ src/main/java/com/helio/demo/DemoController.java
@@ -0,0 +1,13 @@
+package com.helio.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String index() {
+        return "hello there.";
+    }
+}
